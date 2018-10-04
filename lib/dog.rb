8451e9949d96944a13cda88398743cb86414c874
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-10-04 23:07:05 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,17 +10,3 @@ class Dog
   end
 end
 
-class Person
-  def initialize(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
-
-  def name=(new_name)
-    @name = new_name
-  end
-end
-binding.pry
